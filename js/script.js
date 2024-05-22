f0f5697e84937762f153c25ab1d80f5e850bb15a
Jack Bogart <jackbogart0822@gmail.com>
2024-05-21 22:43:45 -0400

    Add function to add books to a library
    Basic function to add a book object to the library array.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -9,3 +11,7 @@ Book.prototype.info = function () {
     const readStr = this.read ? 'read already' : 'not read yet';
     return `${this.title} by ${this.author}, ${this.pages.toString()} pages, ${readStr}`;
 };
+
+function addBookToLibrary(book) {
+    myLibrary.append(book);
+}
