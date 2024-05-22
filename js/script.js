7b58e4628bd4ff9bb36a259b4355f03457ecf2b1
Jack Bogart <jackbogart0822@gmail.com>
2024-05-22 03:31:05 -0400

    Add display cards for book entries
    Book cards will display the information about a book entered into the
    library. To add a book we call the addBookToLibrary() function using a
    book object.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const library = document.querySelector('.books');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -15,3 +16,33 @@ Book.prototype.info = function () {
 function addBookToLibrary(book) {
     myLibrary.append(book);
 }
+
+function displayBook(book) {
+    // Create and assign class for styling
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book');
+
+    // Create separate elements of card
+    const bookTitle = document.createElement('h2');
+    bookTitle.textContent = book.title;
+    bookCard.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = book.author;
+    bookCard.appendChild(bookAuthor);
+
+    const bookPageCount = document.createElement('p');
+    bookPageCount.textContent = `${book.pages} pages`;
+    bookCard.appendChild(bookPageCount);
+
+    const bookRead = document.createElement('p');
+    bookRead.textContent = book.read ? 'read already' : 'not read yet';
+    bookCard.appendChild(bookRead);
+
+    library.appendChild(bookCard);
+}
+
+myLibrary.push(new Book('The Hobbit', 'Tolkien J.R.R.', '295', false));
+myLibrary.push(new Book('Lord of The Rings', 'Tolkien J.R.R.', '428', false));
+myLibrary.push(new Book("Harry Potter and the Sorceror's Stone", 'J.K. Rowling', '328', true));
+myLibrary.forEach((book) => displayBook(book));
