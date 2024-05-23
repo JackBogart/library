6119b4c907abf63a2134c4677675a2e70231d674
Jack Bogart <jackbogart0822@gmail.com>
2024-05-22 23:11:42 -0400

    Add a delete button to book entries
    When hovered over each book entry now displays a delete button to delete
    not only the book card but the book from storage.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const myLibrary = [];
 
 const library = document.querySelector('.books');
 const addBookButton = document.querySelector('.add-book-button');
+const deleteBookButton = document.querySelector('.delete-button');
 
 const bookDialog = document.querySelector('dialog');
 const submitBook = document.querySelector('.submit-book');
@@ -25,14 +26,15 @@ Book.prototype.info = function () {
 };
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
     displayBook(book);
+    myLibrary.push(book);
 }
 
 function displayBook(book) {
     // Create and assign class for styling
     const bookCard = document.createElement('div');
     bookCard.classList.add('book');
+    bookCard.dataset.index = myLibrary.length;
 
     // Create separate elements of card
     const bookTitle = document.createElement('h2');
@@ -50,6 +52,29 @@ function displayBook(book) {
     const bookRead = document.createElement('p');
     bookRead.textContent = book.read ? 'read already' : 'not read yet';
     bookCard.appendChild(bookRead);
+
+    // Adds delete button
+    const deleteButton = document.createElement('button');
+    const trashIcon = document.createElement('img');
+    trashIcon.src = '../img/trash-icon.svg';
+    trashIcon.alt = 'trash button';
+
+    deleteButton.addEventListener('click', (e) => {
+        const bookCard = e.target.closest('.book');
+        const bookIndex = Number(bookCard.dataset.index);
+
+        // Update all data-index values for book cards after the removed
+        for (let i = bookIndex + 1; i < myLibrary.length; i++) {
+            document.querySelector(`.book[data-index="${i}"]`).setAttribute('data-index', i - 1);
+        }
+
+        library.removeChild(bookCard);
+        myLibrary.splice(bookIndex, 1);
+    });
+
+    deleteButton.appendChild(trashIcon);
+    deleteButton.classList.add('delete-button');
+    bookCard.appendChild(deleteButton);
 
     library.insertBefore(bookCard, addBookButton);
 }
@@ -74,7 +99,6 @@ submitBook.addEventListener('click', (e) => {
     }
 });
 
-myLibrary.push(new Book('The Hobbit', 'Tolkien J.R.R.', '295', false));
-myLibrary.push(new Book('Lord of The Rings', 'Tolkien J.R.R.', '428', false));
-myLibrary.push(new Book("Harry Potter and the Sorceror's Stone", 'J.K. Rowling', '328', true));
-myLibrary.forEach((book) => displayBook(book));
+addBookToLibrary(new Book('The Hobbit', 'Tolkien J.R.R.', '295', false));
+addBookToLibrary(new Book('Lord of The Rings', 'Tolkien J.R.R.', '428', false));
+addBookToLibrary(new Book("Harry Potter and the Sorceror's Stone", 'J.K. Rowling', '328', true));
